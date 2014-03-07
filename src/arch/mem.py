a413ccb28ba9b7d58d8456c61b6de8b9bb3eb240
Óscar Gómez <oscar.gomez@uji.es>
2014-03-07 16:51:42 +0100

    Memory añadida

---
+++ src/arch/mem.py
@@ -0,0 +1,41 @@
+"""
+Created on 07/03/2014
+@author: Óscar Gómez Alcañiz <oscar.gomez@uji.es>
+"""
+import app.log
+from app.log import Log
+from datastructures.instruction import Instruction
+
+class InstructionsMemory:
+    def __init__(self):
+        self.instructions = []
+
+    def insert_instruction(self, instruction):
+        self.instructions.append(instruction)
+
+    def get_instruction_at(self, pc):
+        try:
+            return self.instructions[pc]
+        except IndexError:
+            return Instruction()
+            #raise EndOfProgram("No more instructions.")
+
+l = Log("DataMemory")        
+class DataMemory:
+    def __init__(self):
+        self.memory = [word for word in xrange(32)]
+
+    def read_byte(self, index):
+        l.d("Read: {}".format(index), "Memory")
+        return self.memory[index]
+
+    def write_byte(self, index, data):
+        l.d("Write: {} <= {}".format(index, data), "Memory")
+        self.memory[index] = data
+
+    def __str__(self):
+        out = "{}Memory:{}\n".format(app.log.RED_BOLD, app.log.NORMAL)
+        for i, w in enumerate(self.memory):
+            out += "[ {:#04x} ] ".format(w)
+            if i % 8 == 7: out += "\n"
+        return out
